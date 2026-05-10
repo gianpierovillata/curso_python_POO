9c35ae0ff564edf32fef46f665afb60df78cbac0
Piero <ironpiero1@gmail.com>
2026-05-10 08:47:37 -0300

    1- Impresion de libros 2- Uso de dataframes

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ libro1 = Book("Gabriel Garcia Marquez", "Cien Años de Soledad", "978-3-16-14841
 print(libro1)
 
 #definir una lista de libros
-
 libros =[]
 
 libro2 = Book("J.K. Rowling", "Harry Potter y la Piedra Filosofal", "978-3-16-148410-1", "Editorial Salamandra", 1997, True)
@@ -31,4 +30,28 @@ for libro in libros:
     print(f"Nombre: {libro.nombre}, Autor: {libro.autor}, Disponible: {libro.disponible}")
     print("-------------")
 
+
+# informacion de datos del deportista
+
+from pandas import DataFrame
+class DatosSalud:
+    def __init__(self, actividad, tiempo, frecuencia_media, frecuencia_maxima, calorias,distancia):
+        self.actividad = actividad
+        self.tiempo = tiempo
+        self.frecuencia_media = frecuencia_media
+        self.frecuencia_maxima = frecuencia_maxima
+        self.calorias = calorias
+        self.distancia = distancia
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}\nEdad: {self.edad}\nPeso: {self.peso} kg\nAltura: {self.altura} m"
+
+
+datos1 = DatosSalud("Correr", "30 minutos", 120, 150, 300, 5)
+datos2 = DatosSalud("Nadar", "45 minutos", 110, 140, 400, 1)
+
+datos_salud = [datos1, datos2]
+
+df = DataFrame([vars(datos1), vars(datos2)])
+print(df)
 
